6b47726e66f957e5a466056565b7849e33633a23
Luis Float <contact@luisfloat.com>
2022-08-12 22:33:29 -0300

    feat(build): init

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import nodeResolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+const pkg = JSON.parse(fs.readFileSync("package.json"));
+
+export default {
+    input: "./src/main/index.js",
+    output: {
+        file: pkg.main,
+    },
+    external: [
+        "fs",
+        "path"
+    ],
+    plugins: [
+        nodeResolve(),
+        terser(),
+    ],
+};
