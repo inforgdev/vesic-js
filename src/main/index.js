d0c4e087a2889be13ccb2856de84933e490596c6
Luis Float <contact@luisfloat.com>
2022-08-19 19:47:41 -0300

    refactor(main): change `combine/` to `compose/`

--- src/main/index.js
+++ src/main/index.js
@@ -2,7 +2,7 @@ import { vesic, meta } from "./core/index.js";
 import { dummySrc, dummyProc, dummySink } from "./dummy/index.js";
 import { file, mkfile } from "./fs/index.js";
 import { val, ret } from "./js/index.js";
-import { series, parallel } from "./combine/index.js";
+import { series, parallel } from "./compose/index.js";
 import { print } from "./std/index.js";
 
 export {
