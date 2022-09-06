d259423820444f68f3abc536f0a7021ff9ef929c
Luis Float <contact@luisfloat.com>
2022-09-05 23:46:13 -0300

    feat(test): add test for compose functions

---
+++ src/test/compose.test.js
@@ -0,0 +1,22 @@
+import { parallel, series } from "../../dist/vesic.js";
+
+test("compose: `parallel()` should run every sink with the same input", () => {
+    const sink = (input, meta) => input + meta; 
+    const run = (sink) => sink("1", {});
+
+    const auto = run(parallel(sink, sink));
+    const manual = [ run(sink), run(sink) ];
+
+    expect(auto.toString())
+        .toBe(manual.toString());
+});
+
+test("compose: `series()` should run cascading processes and return the last output", () => {
+    const sum = (input, meta) => input + meta.a; 
+
+    const proc = series(sum, sum, sum);
+    const procData = proc(0, { a: 1 });
+
+    expect(procData)
+        .toBe(3);
+});
