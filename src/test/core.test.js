8d3002e76dd80daf1edb820dc176a991983f08a3
Luis Float <contact@luisfloat.com>
2022-09-05 23:51:02 -0300

    feat(test): add test for `vesic()`

---
+++ src/test/core.test.js
@@ -0,0 +1,28 @@
+import { bypass, ret, val, vesic } from "../../dist/vesic.js";
+
+test("core: should run vesic config", () => {
+    const data = "1";
+
+    const vesicConfig = {
+        src: val(data),
+        proc: bypass,
+        sink: ret,
+    };
+    const auto = vesic(vesicConfig);
+
+    expect(vesic(vesicConfig))
+        .toBe(data);
+});
+
+test("core: should run every vesic config", () => {
+    const vesicConfig = {
+        src: val("1"),
+        proc: bypass,
+        sink: ret,
+    };
+    const auto = vesic([ vesicConfig, vesicConfig ]);
+    const manual = [ vesic(vesicConfig), vesic(vesicConfig) ];
+
+    expect(auto.toString())
+        .toBe(manual.toString());
+});
