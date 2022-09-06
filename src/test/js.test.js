900a04be83858372ad3bf2df32e185afc723fb99
Luis Float <contact@luisfloat.com>
2022-09-05 23:55:45 -0300

    feat(test): add test for js functions

---
+++ src/test/js.test.js
@@ -0,0 +1,18 @@
+import { val, ret } from "../../dist/vesic.js";
+
+test("js: `val()` should return a function that return its argument", () => {
+    const data = "1";
+    const src = val(data);
+    const srcData = src();
+
+    expect(srcData)
+        .toBe(data);
+});
+
+test("js: `ret()` should return the function argument", () => {
+    const data = "1";
+    const sinkData = ret(data);
+
+    expect(sinkData)
+        .toBe(data);
+});
