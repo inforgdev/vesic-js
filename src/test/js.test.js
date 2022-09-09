278abdeb8f68010908ac6382950d43ddfe5c605a
Luis Float <contact@luisfloat.com>
2022-09-08 23:57:51 -0300

    refactor(test): fix grammar error in test name

--- src/test/js.test.js
+++ src/test/js.test.js
@@ -1,6 +1,6 @@
 import { val, ret } from "../../dist/vesic.js";
 
-test("js: `val()` should return a function that return its argument", () => {
+test("js: `val()` should return a function that returns its argument", () => {
     const data = "1";
     const src = val(data);
     const srcData = src();
