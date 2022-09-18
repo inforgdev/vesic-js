1cddabd52511418774d92449c0039e795f0a6135
Luis Float <contact@luisfloat.com>
2022-09-18 15:02:04 -0300

    feat(main): return the entire stream in `stream`

--- src/main/core/stream.js
+++ src/main/core/stream.js
@@ -1,35 +1,33 @@
 import { parallel, series } from "../compose/index.js";
 
-function stream() {
+function stream(pipedValue, pipedMeta, pipedSinkVal) {
     return {
-        src: (srcFunc, pipedValue, pipedMeta, pipedSinkVal) => {
+        src(srcFunc) {
             pipedValue = srcFunc();
-
-            return {
-                done: () => pipedSinkVal,
-                meta(metaObj) {
-                    pipedMeta = metaObj;
-                    return this;
-                },
-                proc(procFunc) {
-                    pipedValue = procFunc(pipedValue, pipedMeta || {});
-                    return this;
-                },
-                sink(sinkFunc) {
-                    pipedSinkVal = sinkFunc(pipedValue, pipedMeta || {});
-                    return this;
-                },
-                series(...procFuncs) {
-                    return this.proc(series(...procFuncs));
-                },
-                parallel(...sinkFuncs) {
-                    return this.sink(parallel(...sinkFuncs));
-                },
-                exec(func, meta) {
-                    this.meta(meta)
-                    return this.proc(func)
-                },
-            };
+            return this;
+        },
+        done: () => pipedSinkVal,
+        meta(metaObj) {
+            pipedMeta = metaObj;
+            return this;
+        },
+        proc(procFunc) {
+            pipedValue = procFunc(pipedValue, pipedMeta || {});
+            return this;
+        },
+        sink(sinkFunc) {
+            pipedSinkVal = sinkFunc(pipedValue, pipedMeta || {});
+            return this;
+        },
+        series(...procFuncs) {
+            return this.proc(series(...procFuncs));
+        },
+        parallel(...sinkFuncs) {
+            return this.sink(parallel(...sinkFuncs));
+        },
+        exec(func, meta) {
+            this.meta(meta)
+            return this.proc(func)
         },
     };
 };
