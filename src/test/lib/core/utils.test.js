b26921e7bc089e9240d018a2dc1e2c7ee1c5f46a
Luis Float <contact@luisfloat.com>
2022-12-07 20:19:32 -0300

    refactor(test): move lib tests into `lib/`

--- src/test/lib/core/utils.test.js
+++ src/test/lib/core/utils.test.js
@@ -1,6 +1,6 @@
 import { parallel, series } from "../../dist/vesic.js";
 
-test("compose: `parallel()` should run every sink with the same input", () => {
+test("utils: `parallel()` should run every sink with the same input", () => {
     const sink = (input, meta) => input + meta; 
     const run = (sink) => sink("1", {});
 
@@ -11,7 +11,7 @@ test("compose: `parallel()` should run every sink with the same input", () => {
         .toBe(manual.toString());
 });
 
-test("compose: `series()` should run cascading processes and return the last output", () => {
+test("utils: `series()` should run cascading processes and return the last output", () => {
     const sum = (input, meta) => input + meta.a; 
 
     const proc = series(sum, sum, sum);
