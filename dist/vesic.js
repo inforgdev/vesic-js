c858688e765612865ae431251466b6208d53aaa1
Luis Float <contact@luisfloat.com>
2022-12-08 23:54:16 -0300

    feat(main): add api for task running

--- dist/vesic.js
+++ dist/vesic.js
@@ -1,87 +1,121 @@
-import f, { mkdirSync as m, writeFileSync as p, rmSync as h } from "fs";
-import { format as l, dirname as y } from "path";
-import d from "glob/sync.js";
-function k() {
+import { readFileSync as k, mkdirSync as h, writeFileSync as d, rmSync as y } from "fs";
+import u, { format as S, dirname as v } from "path";
+import p from "glob/sync.js";
+function A() {
   return () => {
   };
 }
-function S(t) {
-  return t;
+function w(r) {
+  return r;
 }
-function b() {
+function E() {
 }
-function j(t) {
-  if (Array.isArray(t))
-    return function(i) {
-      return i.map((o) => j(o));
-    }(t);
-  t = function(i = {}) {
-    return function(o = k, n = S, s = b, c = {}) {
-      i.src = o, i.proc = n, i.sink = s, i.meta = c;
-    }(i.src, i.proc, i.sink, i.meta), i;
-  }(t);
-  let r = t.src, e = t.proc(r, t.meta);
-  return t.sink(e, t.meta);
+function b(r) {
+  if (Array.isArray(r))
+    return function(t) {
+      return t.map((i) => b(i));
+    }(r);
+  r = function(t = {}) {
+    return function(i = A, n = w, a = E, l = {}) {
+      t.src = i, t.proc = n, t.sink = a, t.meta = l;
+    }(t.src, t.proc, t.sink, t.meta), t;
+  }(r);
+  let s = r.src, e = r.proc(s, r.meta);
+  return r.sink(e, r.meta);
 }
-function u(t, r) {
-  return (e) => t(e, r);
+function m(r, s) {
+  return (e) => r(e, s);
 }
-function g(...t) {
-  return (r, e) => t.map((i) => i(r, e));
+function D(...r) {
+  return (s, e) => r.map((t) => t(s, e));
 }
-function w(...t) {
-  return (r, e) => t.reduce((i, o) => o(i, e), r);
+function V(...r) {
+  return (s, e) => r.reduce((t, i) => i(t, e), s);
 }
-function x(t, r, e) {
-  let i, o;
+function _(r, s, e) {
+  let t, i;
   return { src(n) {
-    return t = n, this;
-  }, done: () => e, meta(n) {
     return r = n, this;
+  }, done: () => e, meta(n) {
+    return s = n, this;
   }, useProc(n) {
-    return i = n, this;
-  }, proc(n, s) {
-    return s && this.meta(s), typeof n == "object" && (n = u(i, n)), t = n(t, r || {}), this;
+    return t = n, this;
+  }, proc(n, a) {
+    return a && this.meta(a), typeof n == "object" && (n = m(t, n)), r = n(r, s || {}), this;
   }, useSink(n) {
-    return o = n, this;
-  }, sink(n, s) {
-    return s && this.meta(s), typeof n == "object" && (n = u(o, n)), e = n(t, r || {}), this;
+    return i = n, this;
+  }, sink(n, a) {
+    return a && this.meta(a), typeof n == "object" && (n = m(i, n)), e = n(r, s || {}), this;
   }, series(...n) {
-    return n.forEach((s) => this.proc(s)), this;
+    return n.forEach((a) => this.proc(a)), this;
   }, parallel(...n) {
-    return n.forEach((s) => this.sink(s)), this;
+    return n.forEach((a) => this.sink(a)), this;
   } };
 }
-function P(t) {
-  console.log(t);
+function q(r) {
+  console.log(r);
 }
-function a(t) {
-  return typeof t == "object" ? l(t) : t;
+function f(r) {
+  return typeof r == "object" ? S(r) : r;
 }
-function q(t, r) {
-  return t = a(t || (r == null ? void 0 : r.path)), f.readFileSync(t, (r == null ? void 0 : r.options) || "utf-8");
+function z(r, s) {
+  return r = f(r || (s == null ? void 0 : s.path)), k(r, (s == null ? void 0 : s.options) || "utf-8");
 }
-function z(t, r) {
-  let e = a(r == null ? void 0 : r.path);
-  const i = y(e);
-  return m(i, { recursive: !0 }), p(e, t.toString(), r == null ? void 0 : r.options), t;
+function B(r, s) {
+  let e = f(s == null ? void 0 : s.path);
+  const t = v(e);
+  return h(t, { recursive: !0 }), d(e, r.toString(), s == null ? void 0 : s.options), r;
 }
-function v(t, r) {
-  return Array.isArray(t) ? void t.forEach((e) => v(e)) : (t = a(t), d(t).forEach((e) => {
-    h(e, { recursive: !0, force: !0, ...r == null ? void 0 : r.options });
-  }), t);
+function T(r, s) {
+  return Array.isArray(r) ? void r.forEach((e) => T(e)) : (r = f(r), p(r).forEach((e) => {
+    y(e, { recursive: !0, force: !0, ...s == null ? void 0 : s.options });
+  }), r);
+}
+const g = "./src/task/*.js";
+let o;
+const c = {}, j = (r) => c[r.id] = r, G = (r) => c[r] = void 0, H = (r) => c[r] !== void 0, J = () => process.env.VESIC_API = !0;
+function K(r) {
+  const s = Date.now();
+  c[r].main();
+  const e = Date.now();
+  return { start: s, stop: e, dur: e - s };
+}
+function L(r) {
+  process.env.VESIC_API === "true" ? process.emit("task", r) : r.main();
+}
+function I(r) {
+  const s = { id: u.basename(o || "", u.extname(o || "")), url: o, ...r };
+  j(s);
+}
+function C() {
+  process.listenerCount("task") == 0 && process.on("task", I);
+}
+async function M(r = g) {
+  C();
+  const s = await p(r, {});
+  for (let e of s)
+    o = e, await import(u.resolve(e));
 }
 export {
-  S as bypass,
-  v as clean,
-  b as dummySink,
-  k as dummySrc,
-  u as meta,
-  g as parallel,
-  P as print,
-  q as readFile,
-  w as series,
-  x as stream,
-  j as vesic,
-  z as writeFile
+  w as bypass,
+  T as clean,
+  E as dummySink,
+  A as dummySrc,
+  H as hasTask,
+  J as initApi,
+  C as listenTask,
+  M as loadTasks,
+  m as meta,
+  D as parallel,
+  q as print,
+  z as readFile,
+  j as registerTask,
+  K as runTask,
+  V as series,
+  _ as stream,
+  L as task,
+  c as tasks,
+  G as unregisterTask,
+  b as vesic,
+  B as writeFile
 };
---
+++ src/main/api/task.js
@@ -0,0 +1,62 @@
+import globSync from "glob/sync.js";
+import path from "path";
+import config from "./defaultConfig.js";
+
+let curTaskUrl;
+
+export const tasks = {};
+
+export const registerTask = (task) => tasks[task.id] = task;
+export const unregisterTask = (taskId) => tasks[taskId] = undefined;
+export const hasTask = (id) => tasks[id] !== undefined;
+export const initApi = () => process.env['VESIC_API'] = true;
+
+export function runTask(id) {
+    const start = Date.now();
+    tasks[id].main();
+    const stop = Date.now();
+
+    return {
+        start,
+        stop,
+        dur: stop - start
+    };
+}
+
+export function task(options) {
+    const api = process.env['VESIC_API'] === "true";
+
+    if(!api) {
+        options.main();
+        return;
+    }
+
+    process.emit("task", options);
+}
+
+function handleTask(payload) {
+    const task = {
+        id: path.basename(curTaskUrl || "", path.extname(curTaskUrl || "")),
+        url: curTaskUrl,
+        ...payload,
+    };
+    
+    registerTask(task);
+}
+
+export function listenTask() {
+    if(process.listenerCount("task") == 0) {
+        process.on("task", handleTask);
+    }
+}
+
+export async function loadTasks(glob = config.root) {
+    listenTask();
+
+    const files = await globSync(glob, {});
+
+    for(let file of files) {
+        curTaskUrl = file;
+        await import(path.resolve(file));
+    };
+}
